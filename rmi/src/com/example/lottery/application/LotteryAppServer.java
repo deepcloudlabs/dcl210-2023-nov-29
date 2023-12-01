7eccdb856e167e7407b8ff17f1ec518c3a460d70
Binnur KURT <deepcloudlabs@gmail.com>
2023-12-01 09:33:54 +0300

    add rmi exercise

---
+++ rmi/src/com/example/lottery/application/LotteryAppServer.java
@@ -0,0 +1,19 @@
+package com.example.lottery.application;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+import com.example.lottery.service.business.StandardLotteryService;
+
+public class LotteryAppServer {
+	// JDK = JRE + Monitoring Tools + Development Tools
+	// rmiregistry -> JDK/bin, not in JRE
+    // rmiregistry 9090
+	public static void main(String[] args) throws RemoteException {
+		var lotteryService = new StandardLotteryService();
+		var registry = LocateRegistry.getRegistry(9090);
+        registry.rebind("LotteryService", lotteryService);
+        System.err.println("Lottery RMI Service is running.");
+	}
+
+}
